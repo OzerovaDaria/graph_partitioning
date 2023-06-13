1be1c8b8c499652c97d3cbecf49db47440b05ae4
OzerovaDaria <s02190164@gse.cs.msu.ru>
2023-06-13 21:26:04 +0000

    different group sizes test

--- graph_part.py
+++ graph_part.py
@@ -4,19 +4,21 @@ import nxmetis
 from random import randint
 
 
-def draw_graph(partitioning, alg_name, graph_type):
+def draw_graph(partitioning, alg_name, graph_type, num):
     fig = plt.figure()
     colors, color_map = [], []
     n = len(partitioning)
     for i in range(n):
-        color = '#%06X' % randint(0, 0xFFFFFF)
+        colors.append('#%06X' % randint(0, 0xFFFFFF))
+    for i, c in zip(range(n), colors):
         for node in G:
             if node in partitioning[i]:
-                color_map.append(color)
+                color_map.append(c)
     nx.draw(G, ax=fig.add_subplot(), node_color=color_map, with_labels=True)
-    fig.savefig("graph-" + alg_name + "-" +  graph_type + ".png")
+    fig.savefig("graph-" + alg_name + "-" +  graph_type + "-" + str(num) + ".png")
 
-G = nx.complete_graph(10)
-partitioning = nxmetis.partition(G, 2)[1]
-print(partitioning)
-draw_graph(partitioning, "metis", "complete")
+G = nx.complete_graph(100)
+for num in [2, 4, 10, 20, 25]:
+    partitioning = nxmetis.partition(G, 2)[1]
+    print(num, partitioning)
+    draw_graph(partitioning, "metis", "complete", num)
