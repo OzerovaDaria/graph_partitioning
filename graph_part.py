3cc4f9c65e3c12b708685882d072484cc5c2b434
OzerovaDaria <s02190164@gse.cs.msu.ru>
2023-06-13 21:14:55 +0000

    generate graph + metis + draw

---
+++ graph_part.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import nxmetis
+from random import randint
+
+
+def draw_graph(partitioning, alg_name, graph_type):
+    fig = plt.figure()
+    colors, color_map = [], []
+    n = len(partitioning)
+    for i in range(n):
+        for node in G:
+            if node in partitioning[i]:
+                color_map.append('#%06X' % randint(0, 0xFFFFFF))
+    nx.draw(G, ax=fig.add_subplot(), node_color=color_map, with_labels=True)
+    fig.savefig("graph-" + alg_name + "-" +  graph_type + ".png")
+
+G = nx.complete_graph(10)
+partitioning = nxmetis.partition(G, 2)[1]
+print(partitioning)
+draw_graph(partitioning, "metis", "complete")
